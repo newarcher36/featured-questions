8fc932059ca9a09cd1f9209b1bc24eb46ef4c727
danijm26 <danijm26@gmail.com>
2019-12-04 14:04:10 +0100

    First commit

---
+++ src/main/java/com/flaregames/featuredquestions/FeaturedQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.flaregames.featuredquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturedQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturedQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flaregames/featuredquestions/FeaturedQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaregames.featuredquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturedQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
